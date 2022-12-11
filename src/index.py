2148f3b3471f756005e1489e1dee0ac276dd8124
ivan-abboud <ivan.abboud96@gmail.com>
2022-12-11 16:42:10 +0300

    - fix bugs

--- src/index.py
+++ src/index.py
@@ -61,8 +61,8 @@ def routing(path):
         return about_page_content
 
 
-if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=5050)
+# if __name__ == "__main__":
+#     app.run_server(debug=True, host="0.0.0.0", port=5050)
 
 
 
